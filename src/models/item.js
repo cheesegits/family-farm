c3abb3120dabc4cb9aec2421248c2458e3384b6c
cheesegits <mattmulit@hotmail.com>
2017-05-11 07:51:25 -0400

    Added keys and required values to receiptSchema

--- src/models/item.js
+++ src/models/item.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
 
 var receiptSchema = new mongoose.Schema({
-
+    category: { type: String, required: true },
+    date: { type: Date, required: true },
+    company: { type: String, required: true },
+    item: { type: String, required: true },
+    quantity: { type: String },
+    price: { type: String },
+    tags: { type: [String] }
 });
 
 var Receipt = mongoose.model('Receipt', receiptSchema);
