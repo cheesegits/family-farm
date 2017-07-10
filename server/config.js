f186ed9f46260d8d485c1b0dab15c8041236a3fc
cheesegits <mattmulit@hotmail.com>
2017-07-10 10:08:21 -0400

    not connecting to mLab: redid mLab credentials

--- server/config.js
+++ server/config.js
@@ -2,6 +2,6 @@ exports.DATABASE_URL =
   process.env.DATABASE_URL ||
   global.DATABASE_URL ||
   (process.env.NODE_ENV === "production"
-    ? "mongodb://farmer:mlab@ds027628.mlab.com:27628/family-farm"
+    ? "mongodb://farmer:family@ds027628.mlab.com:27628/family-farm"
     : "mongodb://localhost/familyfarm");
 exports.PORT = process.env.PORT || 8080;
